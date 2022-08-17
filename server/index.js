283e05d10f29cf9ca711f64877f7cfa8d3e449f4
daniel-chowdhury <danielfchowdhury@gmail.com>
2022-08-16 23:49:56 -0400

    Deploying devmountain week 6 day 3 lab second time (this time with github), 8.16.2022

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.get('/',function(req,res) {
+  res.sendFile(path.join(__dirname, '../client/index.html'));
+});
+
+app.get('/css',function(req,res) {
+    res.sendFile(path.join(__dirname, '../client/styles.css'));
+  });
+
+  
+app.get('/js',function(req,res) {
+    res.sendFile(path.join(__dirname, '../client/main.js'));
+  });
+  
+console.log(__dirname)
+const port = process.env.PORT || 4005
+// This will get the PORT variable from Heroku. However, if one isn't assigned (ex. when we are testing locally) it will use port 4005.
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
