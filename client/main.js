bb3c488d8bc6f024ee814fbaaad6a3e40514df7b
daniel-chowdhury <danielfchowdhury@gmail.com>
2022-08-17 00:30:26 -0400

    added picture, linked javascript, added header

--- client/main.js
+++ client/main.js
@@ -1,1 +1,7 @@
 // testing testing
+
+document.querySelector("img").addEventListener("click", displayAlert)
+
+function displayAlert(event) {
+    alert("welcome to my castle")
+}
--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
+app.use(express.json())
+app.use(express.static('client'))
 
 app.get('/',function(req,res) {
   res.sendFile(path.join(__dirname, '../client/index.html'));
