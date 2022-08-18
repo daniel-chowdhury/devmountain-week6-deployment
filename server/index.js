942cb71a6af78ade70a8f9adeadd25ca71b4d653
daniel-chowdhury <danielfchowdhury@gmail.com>
2022-08-18 14:13:54 -0400

    linked rollbar

--- server/index.js
+++ server/index.js
@@ -1,15 +1,31 @@
 const express = require('express')
 const path = require('path')
+//require('dotenv').config()
+
 
 const app = express()
+
+//   '9e7796c1b0914643bccb0be024f8ec9e'
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '9e7796c1b0914643bccb0be024f8ec9e',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+rollbar.log("hello world jk")
+
+
+
 app.use(express.json())
 app.use(express.static('client'))
 
 app.get('/',function(req,res) {
+  rollbar.log("why isnt this working?")
   res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
 app.get('/css',function(req,res) {
+    rollbar.log("they hit css")
     res.sendFile(path.join(__dirname, '../client/styles.css'));
   });
 
@@ -17,6 +33,11 @@ app.get('/css',function(req,res) {
 app.get('/js',function(req,res) {
     res.sendFile(path.join(__dirname, '../client/main.js'));
   });
+
+  // app.get('/rollb', (req, res) => {
+  //   rollbar.log("they hit our website again")
+  //   res.sendFile(path.join(__dirname, '../client/index.html'))
+  // })
   
 console.log(__dirname)
 const port = process.env.PORT || 4005
